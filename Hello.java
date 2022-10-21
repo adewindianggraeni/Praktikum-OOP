ee6078c0497aee00f789270bc9a576d6f51c4bb0
adewindianggraeni <116087061+adewindianggraeni@users.noreply.github.com>
2022-10-22 00:49:03 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Program pertama saya
+ * Author : Windi
+ */
+
+public class Hello {
+		public static void main(String[] args) {
+		// menampilkan tulisan Hello, Ini program pertama Java di Layer
+		System.out.println("Hello, Ini Program Pertama Java");
+	}
+
+}
